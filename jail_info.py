006655f9a8caf1271ab74e8d9f1f1942fb234990
postalove <55823600+postalove@users.noreply.github.com>
2024-03-02 22:00:54 +0800

    Update jail_info.py

--- jail_info.py
+++ jail_info.py
@@ -3,7 +3,7 @@ import os
  
 def load_jailed_members():
     filename = f'{os.path.dirname(__file__)}/jailed_members.json'
-    with open(filename, 'w+') as f:
+    with open(filename, 'r') as f:
         return json.load(f)
 
 

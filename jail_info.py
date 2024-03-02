1a9e6e1b4c9bf48fe3c5158b597db678f6f5fc0c
postalove <55823600+postalove@users.noreply.github.com>
2024-03-02 13:33:35 +0000

    w+

--- jail_info.py
+++ jail_info.py
@@ -3,7 +3,7 @@ import os
  
 def load_jailed_members():
     try:
-        with open(f'{os.path.dirname(__file__)}/jailed_members.json', 'w') as f:
+        with open(f'{os.path.dirname(__file__)}/jailed_members.json', 'w+') as f:
             return json.load(f)
     except FileNotFoundError:
         print('file not found!')
@@ -11,5 +11,5 @@ def load_jailed_members():
 
 # Save jailed members to the file
 def save_jailed_members(jailed_members):
-    with open(f'{os.path.dirname(__file__)}/jailed_members.json', 'w') as f:
+    with open(f'{os.path.dirname(__file__)}/jailed_members.json', 'w+') as f:
         json.dump(jailed_members, f, indent=2)

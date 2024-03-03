8b6cbded2953c995f95ab60e6762ae2d50adf22d
postalove <55823600+postalove@users.noreply.github.com>
2024-03-03 04:13:37 +0000

    .

--- jail_info.py
+++ jail_info.py
@@ -1,17 +1,21 @@
 import json
 from json.decoder import JSONDecodeError
 import os
+import aiofiles
+import asyncio
  
-def load_jailed_members():
-
+async def load_jailed_members():
     try:
-        with open(f'{os.path.dirname(__file__)}/jailed_members.json', 'r') as f:
-            return json.loads(f)
-    except FileNotFoundError or JSONDecodeError:
+        async with aiofiles.open(f'{os.path.dirname(__file__)}/jailed_members.json', 'r') as f:
+            temp_str: str = await f.read()
+            ret_str: str = json.loads(temp_str)
+            return ret_str
+    except FileNotFoundError or JSONDecodeError: #这里注意要有针对空字符串的错误handle
         return {}
 
 
 # Save jailed members to the file
-def save_jailed_members(jailed_members):
-    with open(f'{os.path.dirname(__file__)}/jailed_members.json', 'w') as f:
-        json.dump(jailed_members, f, indent=2)
+async def save_jailed_members(jailed_members):
+    async with aiofiles.open(f'{os.path.dirname(__file__)}/jailed_members.json', 'w') as f:
+        temp_str: str = json.dumps(jailed_members, indent=2)
+        await f.write(temp_str)
